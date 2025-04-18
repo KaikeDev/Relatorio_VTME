135f0314ff123bbbfe0d07d098d016c9f5042c43
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-04-18 14:06:44 -0300

    Alteração na tela relatorio feita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './modules/home/home.component';
 import { EmailComponent } from './modules/emailVtme/email.component';
 
 export const routes: Routes = [
+
+  
   {path: '', component: HomeComponent},
 
 
